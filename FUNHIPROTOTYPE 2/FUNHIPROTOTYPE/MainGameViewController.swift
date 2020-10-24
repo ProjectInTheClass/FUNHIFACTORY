f09b86fbaaaa3c0dab503bbacc4a1a1679ff696c
Son_Hero <heroson0925@gmail.com>
2020-10-24 21:19:48 +0900

    메인 스토리 ui 업데이트

--- FUNHIPROTOTYPE 2/FUNHIPROTOTYPE/MainGameViewController.swift
+++ FUNHIPROTOTYPE 2/FUNHIPROTOTYPE/MainGameViewController.swift
@@ -300,10 +300,10 @@ func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         }
     }
     
-    func displayAnnotation(){
+    /*func displayAnnotation(){
         annotationView.isHidden = false
         annotationText.text = "*ass 123"
-    }
+    }*/
 }
 
 
