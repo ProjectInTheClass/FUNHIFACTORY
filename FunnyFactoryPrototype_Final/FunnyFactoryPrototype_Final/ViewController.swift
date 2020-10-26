f94e462ff0c68b276790c93c2421b1a0a8790b5a
Son_Hero <heroson0925@gmail.com>
2020-10-26 15:49:42 +0900

    new game project file

---
+++ FunnyFactoryPrototype_Final/FunnyFactoryPrototype_Final/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FunnyFactoryPrototype_Final
+//
+//  Created by 손영웅 on 2020/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
