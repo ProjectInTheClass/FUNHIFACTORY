afbd73c8609cb128fff3af919aef0dff473713b4
Son_Hero <heroson0925@gmail.com>
2020-10-28 20:49:57 +0900

    Update constraintCode.swift

--- FUNHIFACTORYPROTOTYPE/FUNHIFACTORYPROTOTYPE/constraintCode.swift
+++ FUNHIFACTORYPROTOTYPE/FUNHIFACTORYPROTOTYPE/constraintCode.swift
@@ -28,3 +28,41 @@ func informationLabelConstraint(titleLabel : UIView, informationLabel : UIView, 
     informationLabel.rightAnchor.constraint(equalTo: note.rightAnchor, constant: 0).isActive = true // ---- 3
     informationLabel.bottomAnchor.constraint(equalTo: note.bottomAnchor, constant: 12).isActive = true // ---- 4
 }
+
+//주석을 모아넣는 스택뷰에 사용할 코드
+    //1개의 노트를 사용할 때
+        func displayOneNote(noteView : UIView, topBar : UIView, exitButton : UIButton, wholeView: UIView){
+            noteView.translatesAutoresizingMaskIntoConstraints = false
+            noteView.topAnchor.constraint(equalTo: topBar.bottomAnchor,constant: 245).isActive = true
+            noteView.bottomAnchor.constraint(equalTo: wholeView.bottomAnchor,constant: -448).isActive = true
+            noteView.centerXAnchor.constraint(equalTo:wholeView.centerXAnchor).isActive = true
+            noteView.frame = CGRect(x: 0, y: 0, width: 268, height: 84)
+            
+            exitButton.translatesAutoresizingMaskIntoConstraints = false
+            exitButton.topAnchor.constraint(equalTo: topBar.bottomAnchor,constant: 183).isActive = true
+            exitButton.rightAnchor.constraint(equalTo: wholeView.rightAnchor, constant: -33).isActive = true
+        }
+    //2개의 노트를 사용할 때
+        func displayDoubleNote(noteView : UIView, topBar : UIView, exitButton : UIButton, wholeView: UIView){
+            noteView.translatesAutoresizingMaskIntoConstraints = false
+            noteView.topAnchor.constraint(equalTo: topBar.bottomAnchor,constant: 181).isActive = true
+            noteView.bottomAnchor.constraint(equalTo: wholeView.bottomAnchor,constant: -312).isActive = true
+            noteView.centerXAnchor.constraint(equalTo:wholeView.centerXAnchor).isActive = true
+            noteView.frame = CGRect(x: 0, y: 0, width: 268, height: 201)
+            
+            exitButton.translatesAutoresizingMaskIntoConstraints = false
+            exitButton.topAnchor.constraint(equalTo: topBar.bottomAnchor,constant: 119).isActive = true
+            exitButton.rightAnchor.constraint(equalTo: wholeView.rightAnchor, constant: -33).isActive = true
+        }
+    //3개의 노트를 사용할 때
+        func displayTripleNote(noteView : UIView, topBar : UIView, exitButton : UIButton, wholeView: UIView){
+            noteView.translatesAutoresizingMaskIntoConstraints = false
+            noteView.topAnchor.constraint(equalTo: topBar.bottomAnchor,constant: 128).isActive = true
+            noteView.bottomAnchor.constraint(equalTo: wholeView.bottomAnchor,constant: -248).isActive = true
+            noteView.centerXAnchor.constraint(equalTo:wholeView.centerXAnchor).isActive = true
+            noteView.frame = CGRect(x: 0, y: 0, width: 268, height: 318)
+            
+            exitButton.translatesAutoresizingMaskIntoConstraints = false
+            exitButton.topAnchor.constraint(equalTo: topBar.bottomAnchor,constant: 66).isActive = true
+            exitButton.rightAnchor.constraint(equalTo: wholeView.rightAnchor, constant: -33).isActive = true
+        }
