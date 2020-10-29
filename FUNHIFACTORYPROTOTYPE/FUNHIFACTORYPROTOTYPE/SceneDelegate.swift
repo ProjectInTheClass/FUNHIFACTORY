7bc1d717f1ecad395ddf44806a6e472cc224a10f
ChoiSeoyeon61 <63771194+ChoiSeoyeon61@users.noreply.github.com>
2020-10-29 11:44:59 +0900

    소월신의 경고 이미지 수정, 경고 아이콘 이미지 로직 추가, 기타 오류 수정

--- FUNHIFACTORYPROTOTYPE/FUNHIFACTORYPROTOTYPE/SceneDelegate.swift
+++ FUNHIFACTORYPROTOTYPE/FUNHIFACTORYPROTOTYPE/SceneDelegate.swift
@@ -16,6 +16,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        UIFont.familyNames.sorted().forEach{ familyName in
+                    print("*** \(familyName) ***")
+                    UIFont.fontNames(forFamilyName: familyName).forEach { fontName in
+                        print("\(fontName)")
+                    }
+                    print("---------------------")
+                }
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
