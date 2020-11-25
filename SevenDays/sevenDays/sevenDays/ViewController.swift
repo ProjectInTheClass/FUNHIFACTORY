9f9a42ff8910c79533d01e7c0697f391f443e625
ChoiSeoyeon61 <63771194+ChoiSeoyeon61@users.noreply.github.com>
2020-11-25 20:32:25 +0900

    .

---
+++ SevenDays/sevenDays/sevenDays/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sevenDays
+//
+//  Created by 최서연 on 2020/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
