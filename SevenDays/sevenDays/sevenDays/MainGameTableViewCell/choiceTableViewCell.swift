42fc61c6f1b83a17b26b78448e903f3d4ad6ee56
Son_Hero <heroson0925@gmail.com>
2020-12-08 01:21:14 +0900

    테이블뷰 선택지 로직 업데이트 1
    선택지 누르면 선택한 선택지 나오고, 선택지 사라지는 거 구현

--- SevenDays/sevenDays/sevenDays/MainGameTableViewCell/choiceTableViewCell.swift
+++ SevenDays/sevenDays/sevenDays/MainGameTableViewCell/choiceTableViewCell.swift
@@ -7,21 +7,31 @@
 
 import UIKit
 
+protocol choiceCellDelegate:AnyObject{
+    func firstChoice()
+    func secondChoice()
+}
 class choiceTableViewCell: UITableViewCell {
     @IBOutlet var firstChoiceButton: UIButton!
     @IBOutlet var secondChoiceButton: UIButton!
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        firstChoiceButton.setTitle(currentBlockOfDay().choices[0].text, for: .normal)
+        secondChoiceButton.setTitle(currentBlockOfDay().choices[1].text, for: .normal)
     }
     func choiceUpdate(first:Choice, second:Choice){
         firstChoiceButton.setTitle(first.text, for: .normal)
         secondChoiceButton.setTitle(second.text, for: .normal)
     }
+    
+    var cellDelegate:choiceCellDelegate?
+    
+    @IBAction func firstButtonTapped(_ sender: Any) {
+        cellDelegate?.firstChoice()
+    }
+    @IBAction func secondButtonTapped(_ sender: Any) {
+        cellDelegate?.secondChoice()
+    }
+    
 }
--- SevenDays/sevenDays/sevenDays/ViewController/mainGameViewController.swift
+++ SevenDays/sevenDays/sevenDays/ViewController/mainGameViewController.swift
@@ -27,9 +27,18 @@ class mainGameViewController: UIViewController, UITableViewDelegate, UITableView
         
         var cellToReturn = UITableViewCell()
         let chatText = currentChatArray[indexPath.row].text
-        print("현재 인덱스넘버 \(indexPath.row)")
         
-        if (currentChatArray[indexPath.row].type == .onlyText||currentChatArray[indexPath.row].type == .textPopup) && currentChatArray[indexPath.row].who.info().name == "키렐"{
+        if indexNumber == currentDay().storyBlocks[player.currentChatId]!.chats.count {
+            print("초이스 시작")
+            let cell = mainGameTableView.dequeueReusableCell(withIdentifier: "choiceTableViewCell", for: indexPath) as! choiceTableViewCell
+            cell.cellDelegate = self
+            print(currentBlockOfDay().choices[0].text)
+            print(currentBlockOfDay().choices[1].text)
+            cell.firstChoiceButton.setTitle(currentBlockOfDay().choices[0].text, for: .normal)
+            cell.secondChoiceButton.setTitle(currentBlockOfDay().choices[1].text, for: .normal)
+            //cell.choiceUpdate(first: currentBlockOfDay().choices[0], second: currentBlockOfDay().choices[1])
+            cellToReturn = cell
+        } else if (currentChatArray[indexPath.row].type == .onlyText||currentChatArray[indexPath.row].type == .textPopup) && currentChatArray[indexPath.row].who.info().name == "키렐"{
             let cell = mainGameTableView.dequeueReusableCell(withIdentifier: "myOnlyText", for: indexPath) as! myOnlyTextTableViewCell
             cell.myChatUpdate(name: currentChatArray[indexPath.row].who.info().name, chat: chatText)
             cell.myProfileUpdate(imageName: currentChatArray[indexPath.row].who.info().profileImage)
@@ -47,7 +56,7 @@ class mainGameViewController: UIViewController, UITableViewDelegate, UITableView
         }
         
         
-        
+        print(currentChatArray)
         return cellToReturn
     }
     
@@ -79,18 +88,49 @@ class mainGameViewController: UIViewController, UITableViewDelegate, UITableView
         pauseBar.isHidden = true
     }
     var timer:Timer!
-    func chatUpdate(){
-        timer = Timer.scheduledTimer(withTimeInterval: 3.0, repeats: true, block: {timer in
-            currentChatArray.append(currentDay().storyBlocks[player.currentChatId]!.chats[self.indexNumber])
-            self.mainGameTableView.insertRows(at: [IndexPath(row: currentChatArray.count-1, section: 0)], with: .automatic)
-            self.indexNumber += 1
-            print("스토리 \(self.indexNumber)/\(currentDay().storyBlocks[player.currentChatId]!.chats.count)")
-            print("1 더함")
-                if self.indexNumber > currentDay().storyBlocks[player.currentChatId]!.chats.count{
+    //채팅 자동으로 올라오게 하기
+    func chatUpdate()
+    {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: {timer in
+            if self.indexNumber < currentDay().storyBlocks[player.currentChatId]!.chats.count{
+                currentChatArray.append(currentDay().storyBlocks[player.currentChatId]!.chats[self.indexNumber])
+                self.mainGameTableView.insertRows(at: [IndexPath(row: currentChatArray.count-1, section: 0)], with: .none)
+                //self.indexNumber += 1
+                print("스토리 \(self.indexNumber)/\(currentChatAmount())")
+            }
+            if self.indexNumber == currentDay().storyBlocks[player.currentChatId]!.chats.count{
                     timer.invalidate()
+                currentChatArray.append(currentDay().storyBlocks[player.currentChatId]!.chats[0])
+                self.mainGameTableView.insertRows(at: [IndexPath(row: currentChatArray.count-1, section: 0)], with: .none)
+                //self.indexNumber += 1
                     return
                 }
+            self.indexNumber += 1
         })
- }
+    }
+    func nextEpisode(){
+        
+    }
+    func nextDay(){
+        
+    }
 }
-
+//선택지 Action 로직
+extension mainGameViewController : choiceCellDelegate{
+    func firstChoice(){
+        player.currentChatId = currentBlockOfDay().choices[0].nextTextId
+        currentChatArray.removeLast()
+        mainGameTableView.deleteRows(at: [IndexPath(row: currentChatArray.count, section: 0)], with: .none)
+        currentChatArray.append(Chat(text: currentBlockOfDay().choices[0].text, image: "", type: .onlyText, who: .kirell, characterFace: true))     //선택지 창이 뜨게 하기 위한 더미 Chat 파일. 직접 보이진 않음.(채우기 용)
+        indexNumber = 0     //indexNumber 초기화
+        mainGameTableView.insertRows(at: [IndexPath(row: currentChatArray.count-1, section: 0)], with: .none)
+        print("First button tapped")
+    }
+    func secondChoice() {
+        player.currentChatId = currentBlockOfDay().choices[1].nextTextId
+        currentChatArray.append(Chat(text: currentBlockOfDay().choices[1].text, image: "", type: .onlyText, who: .kirell, characterFace: true))
+        indexNumber = 0
+        mainGameTableView.insertRows(at: [IndexPath(row: currentChatArray.count-1, section: 0)], with: .none)
+        print("Second button Tapped")
+    }
+}
