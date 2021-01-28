dbd18a0cb1f5159aaec00402969970d7a5ed1747
ChoiSeoyeon61 <63771194+ChoiSeoyeon61@users.noreply.github.com>
2021-01-28 11:37:16 +0900

    .

--- FunhiPrototype2/FunhiPrototype2/Data/UserInstance.swift
+++ FunhiPrototype2/FunhiPrototype2/Data/UserInstance.swift
@@ -12,7 +12,6 @@ import AVFoundation
 var player = User(
     setting: playerSetting,
     currentEpisodes: [prologueChapter,chapter1,chapter2,chapter3,chapter4],
-    currentCharacterInfo: ,
     currentAchievementInfo: [
         Achievement(name: "돈화문", image: "prologue image 1", id: .donhwamun , isLocked: true),
         Achievement(name: "금천교", image: "prologue image 2", id: .geumcheongyo , isLocked: true),
@@ -26,7 +25,7 @@ var player = User(
 
 var playerSetting = Setting(bgmVolume: 0.5, effectVolume: 0.0, textSpeed: 1.0, darkmode: false)
 
-let dummyData = GameData(stories: ["prologue":prologueChapter,"1592":chapter1,"1623":chapter2,"1919":chapter3, "2003":chapter4], histories: ["1":NoteCase(id: .case101, title: "", shortDescription: "", longDescription: "", isLocked: true)], achivements: [:], gameCharacters: )
+let dummyData = GameData(stories: ["prologue":prologueChapter,"1592":chapter1,"1623":chapter2,"1919":chapter3, "2003":chapter4], histories: ["1":NoteCase(id: .case101, title: "", shortDescription: "", longDescription: "", isLocked: true)], achivements: [:] )
 //더미데이터에 들어간 히스토리나 업적들은 오류를 안뜨게 하기 위해서 일부러 넣어놓은 값들. 수정해도 됨.
 
 var timer:Timer!
