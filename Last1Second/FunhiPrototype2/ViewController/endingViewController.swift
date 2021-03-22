f1380c82aaa44cec966b5d8976c4d558f3856dc4
ChoiSeoyeon61 <63771194+ChoiSeoyeon61@users.noreply.github.com>
2021-03-23 08:43:03 +0900

    게임오버뷰 스토리보드 업뎃(코드는 아직)

--- Last1Second/FunhiPrototype2/ViewController/endingViewController.swift
+++ Last1Second/FunhiPrototype2/ViewController/endingViewController.swift
@@ -9,16 +9,34 @@ import UIKit
 
 class endingViewController: UIViewController {
 
+    // MARK: 게임오버 뷰 OUTLET
     @IBOutlet var backgroundImageView: UIImageView!
     @IBOutlet var gameOverImageView: UIImageView!
     @IBOutlet var yearLabel: UILabel!
     @IBOutlet var descriptionLabel: UILabel!
     
+    @IBOutlet var scriptLabel: UILabel!
+    @IBOutlet var sandglassImageView: UIImageView!
+    
+    @IBOutlet var firstButtonImageView: UIImageView!
+    @IBOutlet var secondButtonImageView: UIImageView!
+    @IBOutlet var thirdButtonImageView: UIImageView!
+    
+//MARK: 팝업 뷰 OUTLET
+    
+    @IBOutlet var popupView: UIView!
+    @IBOutlet var popupBox: UIView!
+    @IBOutlet var popupLabel: UILabel!
+    @IBOutlet var popupButton1: UIButton!
+    @IBOutlet var popupButton2: UIButton!
+    @IBOutlet var popupButton1Shadow: UIView!
+    @IBOutlet var popupButton2Shadow: UIView!
     
     
     
-    @IBOutlet var endingType: UILabel!
-    @IBOutlet var endingInformation: UILabel!
+    
+    
+//MARK: 코드
     override func viewDidLoad() {
         super.viewDidLoad()
 
