e96e7b600def975ab9939ca79f5c5c88bf94ad92
Son_Hero <heroson0925@gmail.com>
2021-06-16 16:18:34 +0900

    속마음 셀 추가 수정

--- Last1Second/FunhiPrototype2/ViewController/Cell/TableViewCell/monologueTableViewCell.swift
+++ Last1Second/FunhiPrototype2/ViewController/Cell/TableViewCell/monologueTableViewCell.swift
@@ -28,19 +28,23 @@ class monologueTableViewCell: UITableViewCell {
         var face : String {
             switch profile {
             case .none: return "none"
-            case .angry1: return "danhee angry face1"
-            case .angry2: return "danhee angryface2"
-            case .basic:   return "danhee basic face"
-            case .cry1: return " danhee cry face1"
-            case .cry2: return " danhee cry face2"
-            case .happy1:  return "danhee happy face1"
-            case .happy2:   return "danhee happy face2"
-            case .happy3:   return "danhee happy face3"
-            case .sad:  return "danhee sad face"
-            case .scared:   return "danhee scared face"
-            case .straight: return "danhee straight face"
-            case .surprise1:   return "danhee surprised face1"
-            case .surprise2:    return "danhee surprised face2"
+            case .angry1: return "prologue danhee angry 1"
+            case .angry2: return "prologue danhee angry 2"
+            case .basic:   return "prologue danhee basic"
+            case .cry1: return " prologue danhee cry 1"
+            case .cry2: return " prologue danhee cry 2"
+            case .happy1:  return "prologue danhee happy 1"
+            case .happy2:   return "prologue danhee happy 2"
+            case .happy3:   return "prologue danhee happy 3"
+            case .sad1:  return "prologue danhee sad 1"
+            case .cryangry1: return "prologue danhee sad&angry 1"
+            case .cryangry2: return "prologue danhee sad&angry 2"
+            case .scared:   return "prologue danhee scared"
+            case .straight: return "prologue danhee straight"
+            case .surprise1:   return "prologue danhee surprise 1"
+            case .surprise2:    return "prologue danhee surprise 2"
+            default :
+                return "none"
             }
         }
         if face == "none"{
