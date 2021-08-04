af6b6b2d895ff04b5c21be7c98d4a1d2e453379e
ChoeSeoyeon <choe.seoyeon@mintech.kr>
2021-08-04 22:41:59 +0900

    feat: Gifu 라이브러리 추가

--- Last1Second/FunhiPrototype2/Scene/MainGame/pauseBarAlert.swift
+++ Last1Second/FunhiPrototype2/Scene/MainGame/pauseBarAlert.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Gi
 
 extension mainGameViewController{
     
---
+++ Last1Second/Pods/Target Support Files/Pods-FunhiPrototype2/Pods-FunhiPrototype2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FunhiPrototype2 : NSObject
+@end
+@implementation PodsDummy_Pods_FunhiPrototype2
+@end
