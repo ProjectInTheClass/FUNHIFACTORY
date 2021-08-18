6b9a3a64a6fdef8e846ee2ee2bbac1a79db7669f
Son_Hero <heroson0925@gmail.com>
2021-08-18 15:25:52 +0900

    전각 이미지 터치 시 설명 뜨게 하기

--- Last1Second/FunhiPrototype2/Scene/Map/ImageViewerViewController.swift
+++ Last1Second/FunhiPrototype2/Scene/Map/ImageViewerViewController.swift
@@ -59,22 +59,6 @@ class ImageViewerViewController: UIViewController {
     }
     @objc func showDescription(_ gesture: UITapGestureRecognizer)
     {
-//        let descriptionView = UIView()
-//        let descriptionLabel = UILabel()
-//
-//        descriptionLabel.text = currentSpotData.description
-//        descriptionLabel.textColor = UIColor.white
-//        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-//        descriptionView.addSubview(descriptionLabel)
-//        descriptionLabel.centerXAnchor.constraint(equalTo: descriptionView.centerXAnchor).isActive = true
-//        descriptionLabel.centerYAnchor.constraint(equalTo: descriptionView.centerYAnchor).isActive = true
-//        descriptionView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.8)
-//        descriptionView.tag = 2
-//        self.view.addSubview(descriptionView)
-//        descriptionView.fullScreen(to: spotImage)
-//        descriptionView.translatesAutoresizingMaskIntoConstraints = false
-//        let concern1Gesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(touchDescription(_:)))
-//        descriptionView.addGestureRecognizer(concern1Gesture)
 
     }
     @objc func touchDescription(_ gesture: UITapGestureRecognizer)
@@ -101,6 +85,38 @@ extension   ImageViewerViewController : UICollectionViewDataSource, UICollection
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let size = imageViewCV.frame.size
         return CGSize(width: size.width, height: size.height)
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let descriptionView = UIView()
+        let descriptionLabel = UILabel()
+        let attrString = NSMutableAttributedString(string: currentSpotData.description)
+        let style = NSMutableParagraphStyle()
+
+        style.lineSpacing = 5
+        attrString.addAttribute(NSAttributedString.Key.paragraphStyle, value: style, range: NSMakeRange(0, attrString.length))
+        descriptionLabel.attributedText = attrString
+        descriptionLabel.textColor = UIColor.white
+        descriptionLabel.font = UIFont(name: "NanumSquareR", size: 17)
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.lineBreakMode = .byWordWrapping
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.textAlignment = .center
+        descriptionView.addSubview(descriptionLabel)
+        descriptionLabel.centerXAnchor.constraint(equalTo: descriptionView.centerXAnchor).isActive = true
+//        descriptionLabel.leftAnchor.constraint(equalTo: descriptionView.leftAnchor, constant: 69).isActive = true
+//        descriptionLabel.rightAnchor.constraint(equalTo: descriptionView.rightAnchor, constant: 69).isActive = true
+        descriptionLabel.widthAnchor.constraint(equalToConstant: CGFloat(self.view.bounds.width - 138)).isActive = true
+        descriptionLabel.centerYAnchor.constraint(equalTo: descriptionView.centerYAnchor).isActive = true
+        descriptionLabel.adjustsFontSizeToFitWidth = true
+        descriptionLabel.minimumScaleFactor = 0.5
+        descriptionView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.8)
+        descriptionView.tag = 2
+        self.view.addSubview(descriptionView)
+        descriptionView.fullScreen(to: imageViewCV)
+        descriptionView.translatesAutoresizingMaskIntoConstraints = false
+        let concern1Gesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(touchDescription(_:)))
+        descriptionView.addGestureRecognizer(concern1Gesture)
+
     }
 }
 
